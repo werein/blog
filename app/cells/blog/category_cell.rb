3e7e71cebec427b3948e364666f0f1c9b5b8ca6b
JiriKolarik <jiri.kolarik@imin.cz>
2014-02-17 10:35:32 +0100

    New cell

--- app/cells/blog/category_cell.rb
+++ app/cells/blog/category_cell.rb
@@ -3,11 +3,17 @@ module Blog
     include CanCan::ControllerAdditions
     include Core::ApplicationHelper
     helper Core::ApplicationHelper
+    helper CategoriesHelper
     append_view_path 'app/views'
 
     def show args = {}
       @categories = Blog::Category.roots.with_translations(I18n.locale)
       args[:view] ? render(view: args[:view]) : render unless @categories.empty?
+    end
+
+    def by_name name, args = {}
+      @category = Blog::Category.with_translations(I18n.locale).friendly.find(name)
+      args[:view] ? render(view: args[:view]) : render if @category
     end
   end
 end
--- app/helpers/blog/categories_helper.rb
+++ app/helpers/blog/categories_helper.rb
@@ -1,4 +1,23 @@
 module Blog
   module CategoriesHelper
+
+    def blog_category_tree_with_posts engine_name, items, options = {}
+      content_tag :ul do
+        items.map do |item|
+          output = '<li>'
+          klass = ( item.children.find{|i| current_page?(polymorphic_path([engine_name, i])) } ? 'active' : nil ) if item.has_children?
+          klass = current_page?(polymorphic_path([engine_name, item])) ? 'current' : klass
+          output << link_to(item.send(options.fetch(:title, 'title')), polymorphic_path([engine_name, item]), class: klass)
+          output << blog_category_tree_with_posts(engine_name, item.children, options) if item.has_children?
+          output << content_tag(:ul) do
+            item.posts.map do |post|
+              content_tag(:li, class: 'post') { link_to(post.title, blog.post_path(post)) }
+            end.join.html_safe
+          end if item.posts
+          output << '</li>'
+          output
+        end.join.html_safe
+      end
+    end
   end
 end
