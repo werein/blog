bd3655d45dfded08df02be7cd1582941714bbaf5
Jiri Kolarik <jiri.kolarik@imin.cz>
2014-04-23 12:43:10 +0200

    Require tube rack immediately after rails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,8 @@ Coveralls.wear! if Coveralls.should_run?
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+require 'rails/test_help'
+require 'tuberack'
 
 Rails.backtrace_cleaner.remove_silencers!
 
@@ -31,7 +32,6 @@ class ActionDispatch::IntegrationTest
   include Capybara::DSL
 end
 
-require 'tuberack'
 
 include Tuberack::Helpers
 
