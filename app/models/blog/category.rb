7d2403d9f0f9a3f388611e6ee2cb01f866b9ea3a
Jiri Kolarik <jiri.kolarik@imin.cz>
2014-04-23 12:45:06 +0200

    Rails 4.1 require specify class name in engines

--- app/models/blog/category.rb
+++ app/models/blog/category.rb
@@ -7,7 +7,7 @@ module Blog
     extend FriendlyId
     friendly_id :title, use: :slugged
     
-    has_and_belongs_to_many :posts
+    has_and_belongs_to_many :posts, class_name: 'Blog::Post'
     
     has_ancestry
 
